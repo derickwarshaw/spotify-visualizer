02823ad9375428615652355b0b93df95397c92ef
Kevin Chen <kchen3568@gmail.com>
2017-10-07 23:58:23 -0500

    Add correct javascript to python

--- javascriptpython/jspy.js
+++ javascriptpython/jspy.js
@@ -1,17 +1,18 @@
 var spawn = require('child_process').spawn,
 py = spawn('python', ['parser.py']),
 dataString = new Map()
-var json = require('./dummy.json')
+var json = require('./sample_data.json')
 
 py.stdout.on('data', function(data){
-
 	sets = data.toString().replace(/\s+/g, '').split(";")
 	for(var i=0; i<sets.length; i++) {
 		keyval = sets[i].toString().split(",")
 		dataString.set(Number(keyval[0]),keyval[1])
 	}
-
 })
+// py.stdout.on('error',function(){
+// 	console.log('error!')
+// })
 py.stdout.on('end', function(){
 	console.log(dataString)
 })
