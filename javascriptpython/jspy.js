5aebe60c5cb949f5a7b6c78537367a24896f88aa
Kevin Chen <kchen3568@gmail.com>
2017-10-07 23:02:41 -0500

    Communicate between Node and Python

---
+++ javascriptpython/jspy.js
@@ -0,0 +1,19 @@
+var spawn = require('child_process').spawn,
+py = spawn('python', ['parser.py']),
+dataString = new Map()
+var json = require('./dummy.json')
+
+py.stdout.on('data', function(data){
+
+	sets = data.toString().replace(/\s+/g, '').split(";")
+	for(var i=0; i<sets.length; i++) {
+		keyval = sets[i].toString().split(",")
+		dataString.set(Number(keyval[0]),keyval[1])
+	}
+
+})
+py.stdout.on('end', function(){
+	console.log(dataString)
+})
+py.stdin.write(JSON.stringify(json))
+py.stdin.end()
