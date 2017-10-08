f47eedb7353cd477ff10292dafb04bc1cc3535bf
Kevin Chen <kchen3568@gmail.com>
2017-10-08 03:50:45 -0500

    Add correct timing method

--- timestuff/maptime.js
+++ timestuff/maptime.js
@@ -1,5 +1,3 @@
-
-
 function data(data) {
 	var map = new Map()
 	var keyval = []
@@ -13,31 +11,33 @@ function data(data) {
 map = data("10.55,excite;19.33,relax;21.22,excite;30.29,relax;32.66,excite;55.77,relax;75.37,excite;87.39,relax;88.65,excite;99.63,relax;101.51,excite;106.23,relax;114.0,excite;131.37,excite;141.07,relax;160.71,excite;208.46,relax;214.33,excite;227.78,relax")
 var keys = Array.from(map.keys())
 
-index = 0
-interval = keys[index]*1000
-event = setInterval(function(){
-	if(index!=keys.length-1) {
-		console.log(keys[index],map.get(keys[index++]))
-		//map[keys[index++]]
-	}
-	else
-		clearInterval(event)
-	interval = (keys[index]-keys[index-1])*1000
-	console.log(interval)
-}, interval)
-
 // index = 0
 // interval = keys[index]*1000
-// setTimeout(timeOut(), interval)
+// event = setInterval(function(){
+// 	if(index!=keys.length) {
+// 		console.log(keys[index],map.get(keys[index++]))
+// 		//map[keys[index++]]
+// 	}
+// 	else
+// 		clearInterval(event)
+// 	interval = (keys[index]-keys[index-1])*1000
+// 	console.log(interval)
+// }, interval)
 
-// function timeOut() {
-// 	//doSomething(map.get(keys[index++])) //send what needs to be done on animation to that
-// 	console.log(keys[index],map.get(keys[index++]))
-// 	interval = keys[index]*1000
-// 	if(index!=keys.length)
-// 		setTimeout(timeOut(),interval)
-// }
 
-// function doSomething(event) {
-// 	console.log(event)
-// }
+index = 0
+interval = keys[index]*1000
+
+var TO = function timeOut() {
+	//doSomething(map.get(keys[index++])) //send what needs to be done on animation to that
+	console.log(keys[index],map.get(keys[index++]))
+	interval = (keys[index]-keys[index-1])*1000
+	if(index!=keys.length)
+		setTimeout(TO,interval)
+}
+
+function doSomething(event) {
+	console.log(event)
+}
+
+setTimeout(TO, interval)
