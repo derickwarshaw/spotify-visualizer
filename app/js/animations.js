d24c7c02671659c389a3d01dfaa6d4bb44929218
Varun Rajaram <varun.rajaram@utexas.edu>
2017-10-08 02:51:46 -0700

    It's time to dab

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,13 +1,22 @@
+
+/*
+Initial varable declarations
+*/
 var scene, camera, renderer;
 var WIDTH  = window.innerWidth;
 var HEIGHT = window.innerHeight;
 var SPEED = 0.01;
 var allYPoints = []
 var usefulYCoords = []
+var amplitude = 0.05
+clock = new THREE.Clock()
+
+clock.start();
 
 
-
-
+/*
+Initializer function
+*/
 function init() {
     scene = new THREE.Scene();
     var light = new THREE.DirectionalLight( 0xffffff );
@@ -38,7 +47,7 @@ function initMountain() {
     var loader = new THREE.JSONLoader();
     loader.load('/dist/js/meshes/mountainrange.json', function(geometry, materials) {
         mountain = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(materials, wireframe=true));
-         mountain.scale.x = mountain.scale.y = mountain.scale.z = 0.79;
+        mountain.scale.x = mountain.scale.y = mountain.scale.z = 0.79;
         scene.add(mountain);   
         calcYCoords();
         calcUsefulYCoords();
@@ -66,7 +75,45 @@ function initIsland() {
             })
 }
 
+function data(data) {
+    var map = new Map()
+    var keyval = []
+    var sets = data.toString().replace(/\s+/g, '').split(";")
+    for(var i=0; i<sets.length; i++) {
+        keyval = sets[i].toString().split(",")
+        map.set(Number(keyval[0]),keyval[1])
+    }
+    return map
+}
+map = data("2.55,excite;7.33,relax;21.22,excite;30.29,relax;32.66,excite;55.77,relax;75.37,excite;87.39,relax;88.65,excite;99.63,relax;101.51,excite;106.23,relax;114.0,excite;131.37,excite;141.07,relax;160.71,excite;208.46,relax;214.33,excite;227.78,relax")
+var keys = Array.from(map.keys())
 
+index = 0
+interval = keys[index]*1000
+
+var TO = function timeOut() {
+    doAction(map.get(keys[index]));
+    console.log("fk ur mom")
+    interval = (keys[index]-keys[index-1])*1000
+    if(index!=keys.length)
+        setTimeout(TO,interval)
+}
+setTimeout(TO, interval)
+
+function doAction(param){
+    switch(param) {
+    case "excite":
+        amplitude = 0.1
+        console.log("Excite")        
+        break;
+    case "relax":
+        amplitude = 0.02
+        console.log("relax...")        
+        break;
+    default:
+        break;
+    }
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -85,12 +132,12 @@ function calcYCoords(){
 }
 
 function calcUsefulYCoords() {
-    var yMin = ( Math.max.apply(null, allYPoints) - Math.min.apply(null, allYPoints) ) * 0.8;
+    var yMin = ( Math.max.apply(null, allYPoints) - Math.min.apply(null, allYPoints) ) * 0.7;
     for(var i = 0; i < allYPoints.length; i++){
         if(mountain.geometry.vertices[i].y > yMin) {
             usefulYCoords.push(i)
         }
-    }
+       }
 }
 
 function animateMountainHeight(){
@@ -98,11 +145,9 @@ function animateMountainHeight(){
         return;
     }
     for(var j = 0; j < usefulYCoords.length; j++){
-        if(mountain.geometry.vertices[usefulYCoords[j]].y >= 4){
-        console.log(mountain.geometry.vertices[usefulYCoords[j]].y);
-        mountain.geometry.vertices[usefulYCoords[j]].y += getRandomInt(-4, 4);
-        mountain.geometry.verticesNeedUpdate = true;
-    }
+            var yStep = amplitude * Math.sin(clock.getElapsedTime());
+            mountain.geometry.vertices[usefulYCoords[j]].y += yStep;
+            mountain.geometry.verticesNeedUpdate = true;
     }
 }
 
@@ -110,12 +155,10 @@ function rotateMesh() {
     if (!mountain || !cloud) {
         return;
     }
-    mountain.rotation.y -= SPEED;
-    cloud.rotation.y -= SPEED;
-    island.rotation.y -= SPEED; 
+    //mountain.rotation.y -= SPEED;
+    //cloud.rotation.y -= SPEED;
+    //island.rotation.y -= SPEED; 
 }
-
-
 
 function render() {
     requestAnimationFrame(render);
