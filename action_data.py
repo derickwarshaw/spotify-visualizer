6a80f80d1f45597a9ea57dce44d0b003477d96bf
dkarkada <dkarkada@gmail.com>
2017-10-08 08:43:39 -0500

    it works i think

--- action_data.py
+++ action_data.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct  8 00:01:40 2017
+Created on Fri Oct  6 00:56:38 2017
 
 @author: Dhruva
 """
 import sys
 import json
 import numpy as np
-import matplotlib.pyplot as plot
 
     
 class Segment:
@@ -74,122 +73,93 @@ def stddev(starts, vals, rad):
         ind += 1
     return result
 
-def smooth(starts, vals):
-    slope_threshold = 4
-    flat_threshold = 2
-    result = []
-    window = 3
-    result.append((0, vals[0]))
-    back_ind = 0
-    cur_incline = 0
-    for ind in range(len(starts)):
-        prev_time = result[len(result)-1][0]
-        while(starts[ind] - starts[back_ind+1] > window):
-            back_ind += 1
-        if(starts[back_ind] > prev_time):
-            dy = vals[ind] - vals[back_ind]
-            dx = starts[ind] - starts[back_ind+1]
-            slope = dy/dx
-            if (cur_incline == 0 and abs(slope) > slope_threshold):
-                cur_incline = slope
-                prev_val = result[len(result)-1][1]
-                result.append((starts[back_ind], prev_val))
-                # maybe change back
-            elif (cur_incline != 0 and abs(slope) < flat_threshold):
-                cur_incline = 0
-                result.append((starts[back_ind], vals[back_ind]))
-            elif (cur_incline != 0 and abs(slope) > slope_threshold
-                      and cur_incline * slope < 0):
-                cur_incline = slope
-                result.append((starts[back_ind], vals[back_ind]))
-    return result
-
-        
 def main():
-    filename = "sample_data.json"
-    with open(filename, "r") as my_file:
-        contents = my_file.read()
-        
-# =============================================================================
-#         contents = sys.stdin.readlines()
-# =============================================================================
-        j = json.loads(contents)
-        segments = []
-        for s in j["segments"]:
-            segments.append(Segment(
-                    s["start"],
-                    s["duration"],
-                    s["confidence"],
-                    s["loudness_max"],
-                    s["loudness_max_time"],
-                    s["loudness_start"],
-                    s["timbre"]))
-        
-        plot.figure(figsize=(16,6))
-        plot.axhline(0, color="black")
-        x = [s.start for s in segments]
-        plot.xticks(np.arange(min(x), max(x)+1, 5))
-        
-        y = [s.timbre[1]-s.timbre[2]-s.timbre[4] for s in segments]
-        y_blur = blur(y, 5)
-        y_std_timb = stddev(x, y_blur, 1)
-        mean_timb_std = np.mean(y_std_timb)
-        plot.plot(x, y_std_timb, 'g--')
-        plot.axhline(mean_timb_std, color="green")
-        
-#        step = smooth(x, y_blur)
-#        x_step = [p[0] for p in step]
-#        y_step = [p[1] for p in step]
-#        plot.plot(x_step, y_step, 'k')
-        
-        y = [((0.5)**(-s.max_vol/6))*100 for s in segments]
-        y_blur = blur(y, 5)
-        y_std_vol = stddev(x, y_blur, .75)
-        mean_vol_std = np.mean(y_std_vol)
-        plot.plot(x, y_std_vol, 'r--')
-        plot.axhline(mean_vol_std, color="red")
-        change_regions = []
-        i = 0
-        while i < len(y_std_vol):
-            offset = 0
-            region = []
-            while(y_std_vol[i+offset] > mean_vol_std):
-                region.append(i+offset)                
-                offset += 1
-            if(len(region) > 0):
-                change_regions.append(region)
-            i += offset
-            i += 1
-        criticals = []
-        for region in change_regions:
-            time = x[y_std_vol.index(np.amax([y_std_vol[i] for i in region]))]
-            criticals.append(time)
-        s=""
-        for t in criticals:
-            s += str.format("{},{};", round(t, 2), "AAAA")
-        s = s[:-1]
-        print(s)
-        
-#        step = smooth(x, y_blur)
-#        x_step = [p[0] for p in step]
-#        y_step = [p[1] for p in step]
-#        plot.plot(x_step, y_step, 'k')
-# =============================================================================
-#         actions = {}
-#         for i in range(1, len(step)):
-#             dy = step[i][1] - step[i-1][1]
-#             if(dy > 0):
-#                 actions[step[i][0]] = "excite"
-#             if (dy < 0):
-#                 dx = step[i][0] - step[i-1][0]
-#                 actions[step[i][0] + dx/2] = "relax"
-#         s = ""
-#         for key in actions.keys():
-#             s += str.format("{},{};", round(key, 2), actions[key])
-#         s = s[:-1]
-#         print(s)
-# =============================================================================
+    contents = sys.stdin.readlines()
+    j = json.loads(contents)
+    segments = []
+    for s in j["segments"]:
+        segments.append(Segment(
+                s["start"],
+                s["duration"],
+                s["confidence"],
+                s["loudness_max"],
+                s["loudness_max_time"],
+                s["loudness_start"],
+                s["timbre"]))
     
+
+    x = [s.start for s in segments]
+    
+    y = [s.timbre[1]-s.timbre[2]-s.timbre[4] for s in segments]
+    y_tblur = blur(y, 5)
+    y_std_timb = stddev(x, y_tblur, 1)
+    mean_timb = np.mean(y_std_timb)
+    y_std_timb = [elem/mean_timb for elem in y_std_timb]
+            
+    y = [((0.5)**(-s.max_vol/6))*100 for s in segments]
+    y_vblur = blur(y, 5)
+    y_std_vol = stddev(x, y_vblur, .75)
+    mean_vol = np.mean(y_std_vol)
+    y_std_vol = [elem/mean_vol for elem in y_std_vol]
+    
+    prod = [y_std_timb[i] * y_std_vol[i] for i in range(len(y_std_timb))]
+    
+    change_regions = []
+    i = 0
+    while i < len(prod):
+        offset = 0
+        region = []
+        while(i+offset < len(prod) and prod[i+offset] > 2):
+            region.append(i+offset)                
+            offset += 1
+        if(len(region) > 0):
+            change_regions.append(region)
+        i += offset
+        i += 1
+    criticals = []
+    for region in change_regions:
+        ind = 0
+        for i in region:
+            if(prod[i] > prod[ind]):
+                ind = i
+        val = prod[ind]
+        time = x[ind]
+        if(prod[ind] > 4):
+            criticals.append((time, val))
+    crit_ind = 0
+    crit = criticals[0][0]
+    avgs = []
+    i = 0
+    while(i < len(y_tblur)):
+        offset = 0
+        avg = 0
+        count = 0
+        while(x[i+offset]<crit and i+offset<len(y_tblur)):
+            avg += y_tblur[i+offset]
+            count += 1
+            offset += 1
+        crit_ind += 1
+        crit = criticals[crit_ind][0] \
+                if crit_ind < len(criticals) else x[-1]
+        i += offset + 1
+        avgs.append(avg/count)
+    rng = np.ptp(avgs)
+    down = True
+    actions = []
+    for i in range(len(avgs)-1):
+        cur = avgs[i]
+        nxt = avgs[i+1]
+        if(down and nxt-cur > rng/4):
+            actions.append((criticals[i][0], "excite"))
+            down = False
+        elif(not down and cur-nxt > rng/4):
+            actions.append((criticals[i][0], "relax"))
+            down = True
+    s=""
+    for a in actions:
+        s += str.format("{},{};", round(a[0], 2), a[1])
+    s = s[:-1]
+    print(s)
     
 if __name__ == "__main__":
     main()
