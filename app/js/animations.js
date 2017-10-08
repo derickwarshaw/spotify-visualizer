2726ec8329abb72d4cceb2171d1cb5c010b4c9d4
Varun Rajaram <varun.rajaram@utexas.edu>
2017-10-07 22:26:33 -0700

    Fixed some syntactical errors

--- app/js/animations.js
+++ app/js/animations.js
@@ -2,7 +2,8 @@ var scene, camera, renderer;
 var WIDTH  = window.innerWidth;
 var HEIGHT = window.innerHeight;
 var SPEED = 0.01;
-var zPoints = []
+var allYPoints = []
+var usefulYCoords = []
 
 
 
@@ -15,7 +16,7 @@ function init() {
 
     initMountain();
     initCloud();
-    //initIsland();   
+    initIsland();   
     initCamera();
     initRenderer();
 
@@ -35,14 +36,14 @@ function initRenderer() {
 var mountain = null;
 function initMountain() {
     var loader = new THREE.JSONLoader();
-    loader.load('/dist/js/meshes/mountain.json', function(geometry, materials) {
+    loader.load('/dist/js/meshes/mountainrange.json', function(geometry, materials) {
         mountain = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(materials, wireframe=true));
         mountain.scale.x = mountain.scale.y = mountain.scale.z = 0.79;
         mountain.position.y = 0.5
         mountain.translation = THREE.GeometryUtils.center(geometry);
         scene.add(mountain);
         console.log(mountain)   
-        calcZCoords();
+        calcYCoords();
     });
 }
 
@@ -78,34 +79,33 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-function calcZCoords(){
+function calcYCoords(){
     if(!mountain) {
         console.log('no mountain')
         return;
     }
     console.log(mountain.geometry.vertices.length)
     for(var k = 0; k < mountain.geometry.vertices.length; k++){
-        console.log(mountain.geometry.vertices[k].y)
-        if(mountain.geometry.vertices[k].y > 5) {
-            console.log(mountain.geometry.vertices[k].y)
-            zPoints.push(k)
-        }
+        allYPoints.push(mountain.geometry.vertices[k].y)        
     }
-    console.log(zPoints)  
 }
 
-
+function calcUsefulYCoords() {
+    var yMin = ( Math.max(allYPoints) - Math.min(allYPoints) ) * 0.8;
+    if(mountain.geometry.vertices[k].y > yMin) {
+            usefulYCoords.push(k)
+        }
+}
 
 function animateMountainHeight(){
     if(!mountain){
         return;
     }
 
-    for(var j = 0; j < zPoints.length; j++){
-        mountain.geometry.vertices[zPoints[j]].y += getRandomInt(-3, 3)
+    for(var j = 0; j < usefulYCoords.length; j++){
+        mountain.geometry.vertices[usefulYCoords[j]].y += getRandomInt(-3, 3)
         mountain.geometry.vertices.verticesNeedUpdate = true;
         //console.log(mountain.geometry.vertices[zPoints[j]].z);
-
     }
 }
 
@@ -114,8 +114,8 @@ function rotateMesh() {
         return;
     }
     //mesh.rotation.x -= SPEED * 2;
-    mountain.rotation.y -= SPEED;
-    cloud.rotation.y -= SPEED;
+    //mountain.rotation.y -= SPEED;
+    //cloud.rotation.y -= SPEED;
     //island.rotation.y -= SPEED; 
 
     // for(int i = 0; i < mountain.geometry.vertices.length; i++){
