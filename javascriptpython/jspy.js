a299d590577afd78346eb34dd88c84597e1255fe
Kevin Chen <kchen3568@gmail.com>
2017-10-08 00:17:12 -0500

    Fixed sending algorithm of map

--- javascriptpython/jspy.js
+++ javascriptpython/jspy.js
@@ -1,14 +1,10 @@
 var spawn = require('child_process').spawn,
 py = spawn('python', ['parser.py']),
-dataString = new Map()
+dataString = ''
 var json = require('./sample_data.json')
 
 py.stdout.on('data', function(data){
-	sets = data.toString().replace(/\s+/g, '').split(";")
-	for(var i=0; i<sets.length; i++) {
-		keyval = sets[i].toString().split(",")
-		dataString.set(Number(keyval[0]),keyval[1])
-	}
+	dataString += data.toString()
 })
 // py.stdout.on('error',function(){
 // 	console.log('error!')
--- timestuff/maptime.js
+++ timestuff/maptime.js
@@ -1,4 +1,14 @@
-var map = new Map([[1,'grow'],[3,'stall'].[7,'explosion']])
+var map = new Map()
+
+function data(data) {
+	var keyval = []
+	var sets = data.toString().replace(/\s+/g, '').split(";")
+	for(var i=0; i<sets.length; i++) {
+		keyval = sets[i].toString().split(",")
+		map.set(Number(keyval[0]),keyval[1])
+	}
+}
+
 var keys = map.keys()
 
 index = 0
@@ -14,22 +24,22 @@ event = setInterval(function(){
 
 
 
-var interval = 10;
-var start = Date.now()
-var expected = Date.now() + interval
-setTimeout(step(), interval)
-function step() {
-    var dt = Date.now() - expected // the drift (positive for overshooting)
-    if (dt > interval) {
-        // something really bad happened. Maybe the browser (tab) was inactive?
-        // possibly special handling to avoid futile "catch up" run
+// var interval = 10;
+// var start = Date.now()
+// var expected = Date.now() + interval
+// setTimeout(step(), interval)
+// function step() {
+//     var dt = Date.now() - expected // the drift (positive for overshooting)
+//     if (dt > interval) {
+//         // something really bad happened. Maybe the browser (tab) was inactive?
+//         // possibly special handling to avoid futile "catch up" run
 
-        //not sure what to do here since 10 ms intervals and uhhh 
-    }
-    diff = Date.now()-start
-    if(map.has(diff))
-    	map.get(diff)
+//         //not sure what to do here since 10 ms intervals and uhhh 
+//     }
+//     diff = Date.now()-start
+//     if(map.has(diff))
+//     	map.get(diff)
 
-    expected += interval
-    setTimeout(step(), Math.max(0, interval - dt)) // take into account drift
-}
+//     expected += interval
+//     setTimeout(step(), Math.max(0, interval - dt)) // take into account drift
+// }
