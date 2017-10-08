154705590855b85250b436bc01d9bb0b394889ce
dkarkada <dkarkada@gmail.com>
2017-10-07 22:33:58 -0500

    first draft of excite/relax flags
    some timestamp accuracy, lots of room for improvement lol

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ Created on Fri Oct  6 00:56:38 2017
 
 @author: Dhruva
 """
+import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plot
@@ -69,30 +70,86 @@ def within(seg, beat):
 def blur(vals, rad):
     if(rad == 0 or 2*rad +1 > len(vals)):
         return vals
-    ind = rad;
-    while (ind+rad+1 < len(vals)):
+    ind = 0;
+    while (ind < len(vals)):
         avg = 0
+        count = 0
         for i in range(2*rad + 1):
-            avg += vals[ind-rad+i]
-        avg /= (2*rad+1)
+            if(0 <= ind-rad+i < len(vals)):
+                avg += vals[ind-rad+i]
+                count += 1
+        avg /= count
         vals[ind] = avg
         ind += 1
     return vals
 
-def derivative(vals, rad):
-    if(rad == 0 or rad +1 > len(vals)):
-        return vals
-    ind = 0
-    while (ind+rad+1 < len(vals)):
-        avg = 0
-        for i in range(rad):
-            avg += (vals[ind+i+1] - vals[ind])/(i+1)
-        avg /= rad
-        vals[ind] = avg
-        ind += 1
-    return vals
+def smooth(starts, vals):
+    slope_threshold = 4
+    flat_threshold = 2
+    result = []
+    window = 3
+    result.append((0, vals[0]))
+    back_ind = 0
+    cur_incline = 0
+    for ind in range(len(starts)):
+        prev_time = result[len(result)-1][0]
+        while(starts[ind] - starts[back_ind+1] > window):
+            back_ind += 1
+        if(starts[back_ind] > prev_time + window):
+            dy = vals[ind] - vals[back_ind]
+            dx = starts[ind] - starts[back_ind+1]
+            slope = dy/dx
+            if (cur_incline == 0 and abs(slope) > slope_threshold):
+                cur_incline = slope
+                prev_val = result[len(result)-1][1]
+                result.append((starts[back_ind], prev_val))
+                # maybe change back
+            elif (cur_incline != 0 and abs(slope) < flat_threshold):
+                cur_incline = 0
+                result.append((starts[back_ind], vals[back_ind]))
+            elif (cur_incline != 0 and abs(slope) > slope_threshold
+                      and cur_incline * slope < 0):
+                cur_incline = slope
+                result.append((starts[back_ind], vals[back_ind]))
+    return result
+            
 
 def main():
+    contents = sys.stdin.readLines()
+    j = json.loads(contents)
+    segments = []
+    for s in j["segments"]:
+        segments.append(Segment(
+                s["start"],
+                s["duration"],
+                s["confidence"],
+                s["loudness_max"],
+                s["loudness_max_time"],
+                s["loudness_start"],
+                s["timbre"]))
+    
+    plot.figure(figsize=(16,6))
+    plot.axhline(0, color="black")
+    x = [s.start for s in segments]
+    plot.xticks(np.arange(min(x), max(x)+1, 5))
+    y0 = [s.timbre[0] for s in segments]
+    plot.plot(x, y0, 'k,')
+    s1 = blur([s.timbre[1] for s in segments], 15)
+    s2 = blur([s.timbre[2] for s in segments], 15)
+    s4 = blur([s.timbre[4] for s in segments], 15)
+  #     y = [s1[i]-s2[i]-s4[i] + y0[i] for i in range(len(s1))]
+    y = [s1[i]-s2[i]-s4[i] for i in range(len(segments))]
+    plot.plot(x, y, 'k--')
+    y = blur([((0.5)**(-s.max_vol/6))*100 for s in segments], 5)
+    plot.plot(x, y, 'r--')
+    step = smooth(x, y)
+    x = [p[0] for p in step]
+    y = [p[1] for p in step]
+    plot.plot(x, y, 'k')
+    for p in step:
+        print(p[0], ",", p[1])
+        
+def main1():
     filename = "sample_data.json"
     with open(filename, "r") as my_file:
         contents = my_file.read()
@@ -107,52 +164,35 @@ def main():
                     s["loudness_max_time"],
                     s["loudness_start"],
                     s["timbre"]))
+        
         plot.figure(figsize=(16,6))
+        plot.axhline(0, color="black")
         x = [s.start for s in segments]
         plot.xticks(np.arange(min(x), max(x)+1, 5))
         y0 = [s.timbre[0] for s in segments]
-        plot.plot(x, y0, 'k--')
-        y = [0]*len(x)
-        plot.plot(x, y, 'b-')
+        plot.plot(x, y0, 'k,')
         s1 = blur([s.timbre[1] for s in segments], 15)
         s2 = blur([s.timbre[2] for s in segments], 15)
         s4 = blur([s.timbre[4] for s in segments], 15)
-  #      plot.plot(x, s1, 'r--')
-  #      plot.plot(x, s2, 'b--')
-  #      plot.plot(x, s4, 'g--')
- #       y = [-s2[i] + y0[i] for i in range(len(segments))]
-  #      plot.plot(x, y, 'k')
   #     y = [s1[i]-s2[i]-s4[i] + y0[i] for i in range(len(s1))]
-        y = [s1[i]-s2[i]-s4[i] for i in range(len(s1))]
-        plot.plot(x, y, 'k')
-        y = [s.max_vol for s in segments]
-        plot.plot(x, y, 'r')
+        y = [s1[i]-s2[i]-s4[i] for i in range(len(segments))]
+        plot.plot(x, y, 'k--')
         y = blur([((0.5)**(-s.max_vol/6))*100 for s in segments], 5)
-        plot.plot(x, y, 'r')
-        
-
-# =============================================================================
-#         for s in segments:
-#             if 32 < s.start_time < 32.1:
-#                 print(s.timbre)
-#                 print(round(s.start_time, 3), round(s.max_vol, 2))
-#                 print(s.spectrum)
-#         beats = []
-#         for b in j["beats"]:
-#             beats.append(b["start"] + b["duration"])
-#         ind = 0
-#         for i in range(len(beats)):
-#             beat_segs = []
-#             while(ind < len(segments) and within(segments[ind], beats[i])):
-#                 beat_segs.append(segments[ind])
-#                 ind += 1 
-#             avg_vol = 0
-#             for seg in beat_segs:
-#                 avg_vol += seg.max_vol
-#             if len(beat_segs) is not 0:
-#                 avg_vol /= len(beat_segs)
-#             #print(round(beats[i],2), "\t", avg_vol)
-# =============================================================================
+        plot.plot(x, y, 'r--')
+        step = smooth(x, y)
+        x = [p[0] for p in step]
+        y = [p[1] for p in step]
+        plot.plot(x, y, 'k')
+        actions = {}
+        for i in range(1, len(step)):
+            dy = step[i][1] - step[i-1][1]
+            if(dy > 0):
+                actions[step[i][0]] = "excite"
+            if (dy < 0):
+                dx = step[i][0] - step[i-1][0]
+                actions[step[i][0] + dx/2] = "relax"
+        for key in actions.keys():
+            print(key, ",", actions[key])
     
     
 if __name__ == "__main__":
